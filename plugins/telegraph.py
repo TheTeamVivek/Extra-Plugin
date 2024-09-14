a44861bac441c55ca5d52a11e1367b736983e9ee
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-14 11:46:25 +0530

    Update telegraph.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -1,10 +1,26 @@
 import os
+import json
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from telegraph import upload_file
 from YukkiMusic import app
+import requests
 
+def upload_file(file_path):
+    url = 'https://catbox.moe/user/api.php'
+    data = {
+        'reqtype': 'fileupload',
+        'json': 'true'
+    }
+    files = {
+        'fileToUpload': open(file_path, 'rb')
+    }
+    response = requests.post(url, data=data, files=files)
 
+    if response.status_code == 200:
+        return True, response.text.strip()
+    else:
+        return False, f"ᴇʀʀᴏʀ: {response.status_code} - {response.text}"
+        
 @app.on_message(filters.command(["tgm", "tgt", "telegraph", "tl"]))
 async def get_link_group(client, message):
     if not message.reply_to_message:
@@ -21,8 +37,9 @@ async def get_link_group(client, message):
     elif media.document:
         file_size = media.document.file_size
 
-    if file_size > 5 * 1024 * 1024:
-        return await message.reply_text("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴇᴅɪᴀ ғɪʟᴇ ᴜɴᴅᴇʀ 𝟻MB.")
+    if file_size > 50 * 1024 * 1024:
+        return await message.reply_text("Pʟᴇᴀsᴇ ᴘʀᴏᴠɪᴅᴇ ᴀ ᴍᴇᴅɪᴀ ғɪʟᴇ ᴜɴᴅᴇʀ 50MB.")
+    
     try:
         text = await message.reply("Pʀᴏᴄᴇssɪɴɢ...")
 
@@ -31,25 +48,32 @@ async def get_link_group(client, message):
 
         try:
             local_path = await media.download(progress=progress)
-            await text.edit_text("📤Uᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
-            upload_path = upload_file(local_path)
-            await text.edit_text(
-                f"🌐 | [ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ](https://telegra.ph{upload_path[0]})",
-                reply_markup=InlineKeyboardMarkup(
-                    [
+            await text.edit_text("📤 Uᴘʟᴏᴀᴅɪɴɢ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ...")
+            
+            success, upload_path = upload_file(local_path)
+            
+            if success:
+                await text.edit_text(
+                    f"🌐 | [Uploaded Link]({upload_path})",
+                    reply_markup=InlineKeyboardMarkup(
                         [
-                            InlineKeyboardButton(
-                                "ᴛᴇʟᴇɢʀᴀᴘʜ ʟɪɴᴋ",
-                                url=f"https://telegra.ph{upload_path[0]}",
-                            )
+                            [
+                                InlineKeyboardButton(
+                                    "Uploaded File",
+                                    url=upload_path,
+                                )
+                            ]
                         ]
-                    ]
-                ),
-            )
+                    ),
+                )
+            else:
+                await text.edit_text(f"ᴀɴ ᴇʀʀᴏʀ ᴏᴄᴄᴜʀʀᴇᴅ ᴡʜɪʟᴇ ᴜᴘʟᴏᴀᴅɪɴɢ ʏᴏᴜʀ ғɪʟᴇ\n{upload_path}")
+
             try:
                 os.remove(local_path)
             except Exception:
                 pass
+        
         except Exception as e:
             await text.edit_text(f"❌ Fɪʟᴇ ᴜᴘʟᴏᴀᴅ ғᴀɪʟᴇᴅ\n\n<i>Rᴇᴀsᴏɴ: {e}</i>")
             try:
