2c89833f0c74d8bae608f949f11946a5e186b667
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-27 12:46:22 +0530

    fix

--- truth.py
+++ truth.py
@@ -7,35 +7,35 @@ dare_api_url = "https://api.truthordarebot.xyz/v1/dare"
 
 
 @app.on_message(filters.command("truth"))
-def get_truth(client, message):
+async def get_truth(client, message):
     try:
         response = requests.get(truth_api_url)
         if response.status_code == 200:
             truth_question = response.json()["question"]
-            message.reply_text(f"Truth question:\n\n{truth_question}")
+            await message.reply_text(f"Truth question:\n\n{truth_question}")
         else:
-            message.reply_text(
+            await message.reply_text(
                 "Failed to fetch a truth question. Please try again later."
             )
     except Exception:
-        message.reply_text(
+        await message.reply_text(
             "An error occurred while fetching a truth question. Please try again later."
         )
 
 
 @app.on_message(filters.command("dare"))
-def get_dare(client, message):
+async def get_dare(client, message):
     try:
         response = requests.get(dare_api_url)
         if response.status_code == 200:
             dare_question = response.json()["question"]
-            message.reply_text(f"Dare question:\n\n{dare_question}")
+            await message.reply_text(f"Dare question:\n\n{dare_question}")
         else:
-            message.reply_text(
+            await message.reply_text(
                 "Failed to fetch a dare question. Please try again later."
             )
     except Exception:
-        message.reply_text(
+        await message.reply_text(
             "An error occurred while fetching a dare question. Please try again later."
         )
 
